5c91ce33c96ed7435f291e6b9805277e0389676f
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2024-08-14 18:08:08 -0300

    template cy

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# seletores-cy
+# Seletores alternativos com Cypress
+### QA-Solidário - Seletores avançados no Cypress
+
+## Clonando e executando em sua máquina
+
+### Pré-requisito:
+
+-Node.js - Você encontra em: https://nodejs.org/en/
+
+-Visual Studio Code ou qualquer editor de texto - você encontra em: https://code.visualstudio.com/download
+
+-Git: você encontra em: https://git-scm.com/downloads
+
+
+Via terminal, rode os seguintes comandos:
+```  
+git clone https://github.com/fabioaraujoqa/seletores-cy.git
+```
+```
+cd seletores-cy
+```
+
+#### Para instalar as dependencias:
+```
+npm install 
+```
+
+#### Para executar via Dashboard:
+```
+npx cypress open 
+```
+Após abrir o dashboard, selecione um dos navegadores (De preferencia Electron) e siga com as execuções. 
+
+
+### Bibliotecas de apoio:
+-Cypress: Framework de automação: https://cypress.io/
+
+### Bom exercício ;) 
+Fábio Araújo
+
+
+
+
+
---
+++ cypress/e2e/seletores.cy.js
@@ -0,0 +1,74 @@
+///<reference types = "cypress"/>
+
+describe('Seletores avançados com Cypress', () => {
+
+  beforeEach(() => {
+    cy.visit('../../index.html')
+  });
+
+  it('Seleciona e clica em elementos que contêm um texto específico', () => {
+    //TO DO
+  });
+
+  it('Seleciona e verifica se o checkbox está marcado', () => {
+    //TO DO
+  });
+
+  it('Seleciona um elemento usando atributo data-test e verifica mensagem de feedback', () => {
+    //TO DO
+  });
+
+  it('Seleciona um valor em um select e verifica mensagem de feedback', () => {
+    //TO DO
+  });
+
+  it('Seleciona o primeiro elemento da lista de cachorros usando first()', () => {
+    //TO DO
+  });
+
+  it('Seleciona o último elemento da lista de cachorros usando last()', () => {
+    //TO DO
+  });
+
+  it('Seleciona o terceiro elemento da lista de cachorros usando eq()', () => {
+    //TO DO
+  });
+
+  it('Seleciona todos os elementos filhos dentro do contêiner de cachorros', () => {
+    //TO DO
+  });
+
+  it('Seleciona o elemento pai de um elemento com uma classe específica', () => {
+    //TO DO
+  });
+
+  it('Seleciona o próximo irmão do terceiro elemento da lista de gatos', () => {
+    //TO DO
+  });
+
+  it('Seleciona o irmão anterior do terceiro elemento da lista de gatos', () => {
+    //TO DO
+  });
+
+  it('Seleciona o irmão anterior da div de gatos', () => {
+    //TO DO
+  });
+
+  it('Seleciona um elemento com ID e classe específicos', () => {
+    //TO DO
+  });
+
+  it('Seleciona o segundo elemento da lista de cachorros usando nth-child', () => {
+    //TO DO
+  });
+
+  it('Seleciona o primeiro e último elemento da lista de cachorros usando pseudo-classes CSS', () => {
+    //TO DO
+  });
+
+  it('Filtra e seleciona elementos que contêm a palavra "Bulldog"', () => {
+    //TO DO
+  });
+
+
+});
