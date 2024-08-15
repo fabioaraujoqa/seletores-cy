84e058a61d2d31b59ccb54cce0b07741518b8f3d
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2024-08-15 13:23:34 -0300

    testes seletores

--- cypress/e2e/seletores.cy.js
+++ cypress/e2e/seletores.cy.js
@@ -7,68 +7,80 @@ describe('Seletores avançados com Cypress', () => {
   });
 
   it('Seleciona e clica em elementos que contêm um texto específico', () => {
-    //TO DO
+    cy.get('[value="Enviar"]').click()
+    cy.contains('Aceito os').click()
   });
 
   it('Seleciona e verifica se o checkbox está marcado', () => {
-    //TO DO
+    cy.get('[id="aceitar-termos"]').check().should('be.checked')
   });
 
   it('Seleciona um elemento usando atributo data-test e verifica mensagem de feedback', () => {
-    //TO DO
+    cy.get('[data-test="enviar"]').click()
+    cy.get('[id="mensagem-feedback"]').should('contain' , 'Obrigado por compartilhar conosco!')
   });
 
   it('Seleciona um valor em um select e verifica mensagem de feedback', () => {
-    //TO DO
+    cy.get('[name="opcao"]').select('Gato')
   });
 
+  //Medios
+
   it('Seleciona o primeiro elemento da lista de cachorros usando first()', () => {
-    //TO DO
+    cy.get('.raca-cachorro').first().should('contain', 'Golden Retriever')
   });
 
   it('Seleciona o último elemento da lista de cachorros usando last()', () => {
-    //TO DO
+    cy.get('.raca-cachorro').last().should('contain', 'Rottweiler')
   });
 
   it('Seleciona o terceiro elemento da lista de cachorros usando eq()', () => {
-    //TO DO
+    cy.get('.raca-cachorro').eq(2).should('contain', 'Labrador')
   });
 
   it('Seleciona todos os elementos filhos dentro do contêiner de cachorros', () => {
-    //TO DO
+    cy.get('[data-test="div-cachorros"]').find('li')
   });
 
   it('Seleciona o elemento pai de um elemento com uma classe específica', () => {
-    //TO DO
+    cy.get('#gato-3').parent('[name="gatos"]')
   });
 
   it('Seleciona o próximo irmão do terceiro elemento da lista de gatos', () => {
-    //TO DO
+    cy.get('#gato-3').next()
   });
 
-  it('Seleciona o irmão anterior do terceiro elemento da lista de gatos', () => {
-    //TO DO
+  it.only('Seleciona o irmão anterior do terceiro elemento da lista de gatos', () => {
+    cy.get('#gato-3').prev()
   });
-
+  
   it('Seleciona o irmão anterior da div de gatos', () => {
-    //TO DO
+    cy.get('.container-gatos').prev().should('contain', 'SRD Caramelo')
   });
 
   it('Seleciona um elemento com ID e classe específicos', () => {
-    //TO DO
+    cy.get('#id-item-especial.item-isolado').should('contain', 'SRD Caramelo')
   });
 
   it('Seleciona o segundo elemento da lista de cachorros usando nth-child', () => {
-    //TO DO
+    cy.get('li:nth-child(2)').should('contain', 'Bulldog Francês')
   });
 
   it('Seleciona o primeiro e último elemento da lista de cachorros usando pseudo-classes CSS', () => {
-    //TO DO
+    cy.get('li:first-of-type').should('contain', 'Golden Retriever')
+    cy.get('li:last-of-type').should('contain', 'Rottweiler')
   });
 
   it('Filtra e seleciona elementos que contêm a palavra "Bulldog"', () => {
-    //TO DO
+    cy.get('.raca-cachorro').filter(':contains("Bulldog")')
+      .should('have.length', 1)
+      .and('contain', 'Bulldog Francês')
   });
 
+  it('Seleciona e verifica o conteúdo de todos os elementos <li> da lista de cachorros', () => {
+    cy.get('li').each(($el, index, $list) => {
+      expect($el.text()).to.match(/(Golden Retriever|Bulldog Francês|Labrador|Poodle|Beagle|Chihuahua|Dachshund|Pastor Alemão|Boxer|Rottweiler)/)
+    });
+  });
 
 });
